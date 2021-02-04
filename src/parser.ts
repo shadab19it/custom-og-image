e5fe77caa002eba70f0efbb475dc84804b931bd3
Shadab <shadab19it@gmail.com>
2021-02-05 02:49:22 +0530

    make card with custom url

---
+++ src/parser.ts
@@ -0,0 +1,38 @@
+import { IncomingMessage } from "http";
+import { parse } from "url";
+
+interface ParserReqs {
+  author: string;
+  title: string;
+  image: string;
+  website: string;
+}
+
+export const parseReqs = (req: IncomingMessage) => {
+  const { query = {} } = parse(req.url || "", true);
+  const { author, title, image, website } = query;
+
+  if (Array.isArray(author)) {
+    throw new Error("Author must be string");
+  }
+  if (Array.isArray(title)) {
+    throw new Error("Title must be string");
+  }
+  if (Array.isArray(image)) {
+    throw new Error("image must be string");
+  }
+  if (Array.isArray(website)) {
+    throw new Error("WebSite must be string");
+  }
+
+  const parseReqs = {
+    author,
+    title,
+    image,
+    website,
+  };
+
+  console.log(JSON.stringify(parseReqs));
+
+  return parseReqs;
+};
